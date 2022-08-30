8b21422b1c856cb97ff88b99956c8d23c4baded4
Luis-Donato-Martinez-Simuta <74307313+Luis-Donato-Martinez-Simuta@users.noreply.github.com>
2022-08-30 12:17:37 -0500

    Add files via upload

--- app.server.js
+++ app.server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const cors = require('cors');
 
 const user = require('./routes/users.routes');
 const client = require('./routes/client.routes');
@@ -21,10 +21,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json({limit: '50mb'}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
-app.use('/api/users', user);
+app.use('/api/user', user);
 app.use('/api/client', client);
-app.use('/api/state', state);
+app.use('/api/states', state);
 app.use('/api/municipality', municipality);
 
 // catch 404 and forward to error handler
